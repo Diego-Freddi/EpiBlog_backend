984aecd05e2fe791ecc710971418e920ea82692c
Diego F <diego.freddi@icloud.com>
2025-03-27 20:09:01 +0100

    Sistemazione del CORS in server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // middleware
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://epiblog-seven.vercel.app',
     credentials: true
 }));
 app.use(express.json());
